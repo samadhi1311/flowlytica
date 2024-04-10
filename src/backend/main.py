bd8561f3453290318a3da80bb55c8aefe7aaaf94
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-04-10 17:13:43 +0530

    Added form submission via POST

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,7 @@
 # Filename - main.py
 
 # Import flask
-from flask import Flask, jsonify
+from flask import Flask, json, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -17,6 +17,26 @@ def users():
             ]
 		}
 	)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+
+    # Retrieve form data from the request
+    form_data = request.json
+    
+    # Process the form data
+    url = form_data.get('url')
+    count = form_data.get('count')
+    content_text = form_data.get('content_text')
+    content_images = form_data.get('content_images')
+
+    print('url: ', url)
+    print('count: ', count)
+    print('content_text: ', content_text)
+    print('content_images: ', content_images)
+
+    return jsonify({'message': 'Data received successfully'})
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=812)
