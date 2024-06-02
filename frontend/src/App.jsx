02f0df93efd35ee678143e4bca32f65eed1eed63
SandunikaT <tharanisandunika1997@gmail.com>
2024-06-02 20:58:36 +0530

    Added visualization for results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import logo from './assets/flowlytica-mono.svg';
+import Loader from './components/Loader/Loader';
 import './App.css';
 import axios from 'axios';
 
 function App() {
 	const error = 'Provide a valid URL';
+	const [loading, setLoading] = useState(false);
 
 	// Form data object with fallback values
 	// https://www.pinterest.com/sithruby/handicraft/
@@ -17,22 +19,40 @@ function App() {
 		images: false,
 	});
 
-	const handleSubmit = (event) => {
-		console.log(formData);
+	// Userdata object
+	const [userData, setUserData] = useState({
+		avatar: '',
+		username: '',
+		board: '',
+		pins: '',
+		captions: [],
+	});
+
+	const handleSubmit = (values) => {
+		setLoading(true);
+		console.log(values);
 
 		// Make post request to backend
 		axios
-			.post('http://localhost:812/analyze', formData)
+			.post('http://localhost:812/analyze', {
+				url: values.url,
+				count: values.count,
+				text: values.text ? true : false,
+				images: values.images ? true : false,
+			})
 			.then((response) => {
 				console.log(response.data);
+				setUserData(response.data);
+				setLoading(false);
 			})
 			.catch((error) => {
 				console.error(error);
+				setLoading(false);
 			});
 
 		// Scroll to results
-		// let resultsSection = document.getElementById('results');
-		// resultsSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+		let resultsSection = document.getElementById('results');
+		resultsSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
 	};
 
 	return (
@@ -45,30 +65,7 @@ function App() {
 			</section>
 
 			<section className='input-section'>
-				<Formik
-					initialValues={formData}
-					onSubmit={(values) => {
-						console.log(values);
-
-						// Make post request to backend
-						axios
-							.post('http://localhost:812/analyze', {
-								url: values.url,
-								count: values.count,
-								text: values.text ? true : false,
-								images: values.images ? true : false,
-							})
-							.then((response) => {
-								console.log(response.data);
-							})
-							.catch((error) => {
-								console.error(error);
-							});
-
-						// Scroll to results
-						// let resultsSection = document.getElementById('results');
-						// resultsSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
-					}}>
+				<Formik initialValues={formData} onSubmit={handleSubmit}>
 					<Form className='form-container' autoComplete='off'>
 						<div className='form'>
 							<div className='platform-options'>
@@ -121,12 +118,28 @@ function App() {
 			</section>
 
 			<section className='results' id='results'>
-				<div>
-					{/*array.map((item, index) => (
-						<p key={index}>{item}</p>
-					))
-					*/}
-				</div>
+				{loading ? (
+					<Loader />
+				) : (
+					<div className='pinterest'>
+						<div className='pin-header'>
+							<img src={userData.avatar} alt='user avatar' className='pin-avatar' />
+							<span className='pin-author'>
+								<h1 className='pin-heading'>{userData.board}</h1>
+								<h3 className='pin-username'>
+									by <a href={`https://www.pinterest.com/${userData.username}`}>{userData.username}</a>
+								</h3>
+								<p className='pin-count'>{userData.pins} Pins</p>
+							</span>
+						</div>
+						<div className='pin-keywords'>
+							<h2 className='pin-keywords-heading'>Top keywords</h2>
+							<div className='pin-keywords'>
+								<ol>{userData.captions && userData.captions.map((item, index) => <li key={index}>{item.caption}</li>)}</ol>
+							</div>
+						</div>
+					</div>
+				)}
 			</section>
 		</main>
 	);
