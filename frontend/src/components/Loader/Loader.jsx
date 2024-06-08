960988ff24a72897128447941daa074a74c5d4e8
Mithini <mithiniugc@gmail.com>
2024-06-08 13:28:07 +0530

    Added Loading screen

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -3,8 +3,8 @@ import './Loader.css';
 function Loader() {
 	return (
 		<div className='loader-container'>
-			<div class='spinner'>
-				<div class='spinner-inner'></div>
+			<div className='spinner'>
+				<div className='spinner-inner'></div>
 			</div>
 			<p>Analyzing...</p>
 		</div>
